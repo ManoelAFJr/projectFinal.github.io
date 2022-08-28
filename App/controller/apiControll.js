0e2bb99318b5206152ee2dffccc40041df4c3e4f
Manoel Junior <manoeljunior06avelino@gmail.com>
2022-08-28 20:52:42 -0300

    refatorando as routers

---
+++ App/controller/apiControll.js
@@ -0,0 +1,145 @@
+const { json } = require('body-parser');
+const { error } = require('express-openapi-validator');
+const passport = require('passport');
+const User = require('../model/user');
+const neo4j = require('../data/neo4j');
+
+//apiLogin post('/api/login')
+const apiLogin = (req, res)=>{
+  passport.authenticate('login',{
+    successRedirect: res.status(200).json('sucess'),
+    failureRedirect: res.status(400).json('error')
+  })
+}
+
+//apiUserUsername get('/api/user/:username')
+const apiUserUsername =  async(req, res, next)=>{
+  const pessoa = await User.find({username: req.params.username});
+  if(pessoa.length > 0){
+      res.status(200).send(pessoa)
+  }else{
+      res.status(400).json('Usuario nao encontrada');
+ }
+}
+
+//delet.post('/api/delete/:email)
+const apiDelete =  (req, res, next) =>{
+  const email = req.params.email;
+  User.deleteOne({ email : email}, (err, user)=>{
+    if(err){
+      return next(err);
+    } 
+    if(!user){
+      return next(404);
+    }
+    res.status(200).json('deu certo');
+  });
+}
+
+//delet.post('/api/delete/:_id')
+const apiDeleteId = (req, res, next) =>{
+  const id = req.params._id;
+  User.deleteOne({ id : id}, (err, user) =>{
+    if(err){
+      return next(err);
+    } 
+    if(!user){
+      return next(404);
+    }
+    res.status(200).json('deu certo');
+  });
+}
+
+//edit.post('/api/edite')
+const apiEdite =  async (req, res) =>{
+  const email = req.body.email;
+  const age = req.body.age;
+  const username = req.body.username;
+  const address = {
+    city : req.body.city,
+    road : req.body.road,
+    district : req.body.district,
+    zip : req.body.zip
+  }
+  const result =  await User.updateOne({email: email}, 
+    {$set:{username:username, age:age, address:address }}, {upsert:true});
+    if(result.modifiedCount > 0){
+      res.status(200).json();
+    }
+    res.status(400).json();
+}
+
+ //forum.post('/api/forum')
+ const apiForum =  (req, res, next) => {
+  req.user.msg = req.body.message;
+  req.user.save( (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.status(200).json(message);
+  });
+}
+
+//login.get("/api/logout")
+const apiExit = (req, res, next) =>{
+	req.logout(req.user, err => {
+    console.log(logout);
+		if(err) return next(err);
+		res.status(200).json('logout');
+	});
+}
+
+//profile.get('/api/user')
+const listUser = (req, res, next)=>{
+  User.find().sort({createdAt: 'descending'}).exec((err, users)=>{
+    if (err){
+      return next(err);
+    }
+    res.status(200).send(users);
+  });
+};
+
+//register.post('/api/register') 
+const apiRegister = (req, res, next) =>{
+  const name = {
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+  }; 
+  const age = req.body.age;
+  const username = req.body.username;
+  const blood = req.body.blood;
+  const email = req.body.email;
+  const password = req.body.password;
+  const address = {
+    city : req.body.city,
+    road : req.body.road,
+    district : req.body.district,
+    zip : req.body.zip
+  };
+  
+  User.findOne({email: email}, (err, user)=>{
+    if (err){
+      return next(err);
+    }
+    if(user){
+      return res.status(401).json(user);
+    }
+
+    const newUser = new User({
+      name : name,
+      username: username,
+      blood: blood,
+      age : age,
+      email: email,
+      password : password,
+      address : address,
+    });
+    newUser.save(next);
+    return res.json(newUser);
+  }); 
+}
+
+module.exports = {apiDelete, apiDeleteId, apiEdite, 
+  apiLogin, apiForum, apiUserUsername, apiExit, 
+  listUser, apiRegister}
---
+++ App/controller/userControll.js
@@ -0,0 +1,200 @@
+const passport = require('passport');
+const User = require('../model/user');
+const neo4j = require('../data/neo4j');
+
+
+//list users get('/user')
+const users = (req, res, next) =>{
+    User.find().sort({createdAt: 'descending'}).exec((err, users)=>{
+        if (err){
+          return next(err);
+        }
+        res.render('profiles', {users: users});
+      });
+}
+
+//list users get('/user/forum)
+const userForum = (req, res, next) =>{
+	User.find().sort({createdAt: 'descending'}).exec((err, users)=>{
+			if (err){
+				return next(err);
+			}
+			res.render('forum', {users: users});
+		});
+}
+
+//list user get('/users/:username')
+const user = async(req, res, next) =>{
+	User.findOne({ username: req.params.username }, async (err, user) =>{
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return next(404);
+		}
+		const session = neo4j.session();
+		const result = await session.run(
+				 `MATCH (p:Pessoa {username:"${req.params.username}"})-[r:DOAR]->(p2:Pessoa) RETURN p2.username, r.date`
+		);
+		const persons =  (result.records || []).map(obj => {
+			return {username: obj._fields[0], date: obj._fields[1]}
+		}) 
+		session.close();
+		res.render('profile', { user: user , donors: persons});
+	});
+}
+
+//register
+const singup = (req, res, next) =>{
+	const name = {
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+  }; 
+  const age = req.body.age;
+  const username = req.body.username;
+  const blood = req.body.blood;
+  const email = req.body.email;
+  const password = req.body.password;
+  const address = {
+    city : req.body.city,
+    road : req.body.road,
+    district : req.body.district,
+    zip : req.body.zip
+  };
+  
+  User.findOne({email: email}, (err, user)=>{
+    if (err){
+      return next(err);
+    }
+    if(user){
+      req.flash('error', 'User existent!');
+      return res.redirect('/register');
+    }
+
+    const newUser = new User({
+      name : name,
+      username: username,
+      age : age,
+      blood: blood,
+      email: email,
+      password : password,
+      address : address,
+    });
+    newUser.save().then(async()=>{
+      const session = neo4j.session({ database: 'neo4j' });
+      await session.run(`CREATE (p:Pessoa {username:"${req.body.username}"})`);
+      await session.close();
+      next();
+    });
+  });
+}
+
+//edite update post('/edite')
+const edite = (req, res, next) =>{
+	const originalName = req.user.username;
+    req.user.username = req.body.username;
+    req.user.age = req.body.age;
+    req.user.address = {
+      city : req.body.city,
+      road : req.body.road,
+      district : req.body.district,
+      zip : req.body.zip
+    }
+    req.user.save(function (err) {
+      if (err) {
+        next(err);
+        return;
+      }
+      if(originalName != req.user.username){
+        const session = neo4j.session();
+        session.run(`MATCH (p:Pessoa {username: "${originalName}"}) SET p.username = "${req.user.username}"`)
+      }
+      req.flash('info', 'Upgrade sucess!');
+      res.redirect('/tech');
+    });
+}
+
+const editeUsername = async(req, res, next) =>{
+  User.findOne({ username: req.params.username }, async function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(404);
+    }
+    const session = neo4j.session();
+    const result = await session.run(
+         `MATCH (p:Pessoa {username:"${req.params.username}"})-[r:DOAR]->(p2:Pessoa) RETURN p2.username, r.date`
+    );
+    const persons =  (result.records || []).map(obj => {
+      return {username: obj._fields[0], date: obj._fields[1]}
+    }) 
+    session.close();
+    res.render('profile', { user: user , donors: persons});
+  });
+}
+
+//delet delete post('/delete')
+const delet = (req, res, next) =>{
+	const email = req.body.email;
+  const id = req.body._id;
+   User.deleteOne({email: email, id: id}, (err) =>{
+     if(err){
+       next(err);
+       return
+     }
+     if(User){
+       req.flash('error', 'User not found');
+       return res.redirect('/delete');
+     }
+      return res.redirect('/');
+   });
+} 
+
+//criando relacionamento get('/users/:username1/donate/:username2')
+const donors = async(req, res, next) =>{
+	const session = neo4j.session();
+  const result = await session.run(
+       `MATCH (p2:Pessoa{username:"${req.params.username1}"})`+
+       `OPTIONAL MATCH (p1:Pessoa{username:"${req.params.username2}"})`+
+       ` CREATE (p1)-[:DOAR {date:date()}]->(p2) `+
+       `RETURN p2`);
+   await session.close();
+  if(result.summary.counters._stats.relationshipsCreated > 0){
+       return res.redirect('/user');
+  }else{
+   req.flash("error", 'tente novamente mais tarde');
+   return res.redirect('/user');
+  }
+    
+}
+
+//login post('/login')
+const log = passport.authenticate('login', {
+	successRedirect: '/tech',
+  failureRedirect: '/login',
+  failureFlash: true,
+});
+
+//logout get('/logout')
+const exit = (req, res) =>{
+	req.logout(req.user, err => {
+		if(err) return next(err);
+		res.redirect("/");
+	});
+}
+
+//forum post('forum)
+const forum = (req, res, next) =>{
+	req.user.msg = req.body.message;
+  req.user.save(function (err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect('/user/forum');
+  });
+}
+
+
+module.exports = { users, user, userForum, singup, edite, editeUsername, delet, donors, log, exit, forum };
