9d5251e7eac48c9deb4aa2681f5b6f2317a28fcd
Manoel Junior <manoeljunior06avelino@gmail.com>
2022-08-31 08:30:57 -0300

    Atlas mongoose

--- App/data/mongo.js
+++ App/data/mongo.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 
 main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:`+
-  `${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
-}
-
 // async function main() {
-//     await mongoose.connect(
-//     `mongodb+srv://${process.env.M_USER}:`+
-//     `${process.env.M_PASSWORD}`+
-//     `@${process.env.M_HOST}/${process.env.M_PORT}`);
-//   }
+//   await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:`+
+//   `${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
+// }
+
+async function main() {
+    await mongoose.connect(
+    `mongodb+srv://${process.env.M_USER}:`+
+    `${process.env.M_PASSWORD}`+
+    `@${process.env.M_HOST}/${process.env.M_PORT}`);
+  }
 
 
 module.exports = mongoose;
